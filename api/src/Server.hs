2c553e748b7e00a349363ff888b6207f575b7ccb
Suphakorn Homnan <suphakornhomnan@Suphakorns-MacBook-Pro.local>
2021-03-12 00:57:41 +0700

    [Changed] invest api

--- api/src/Server.hs
+++ api/src/Server.hs
@@ -51,7 +51,8 @@ type UserAPI = "api":>"saving" :> ReqBody '[JSON] SavingInfo :> Post '[JSON] Res
 data SavingInfo = SavingInfo
   {
     value :: Float,
-    multi :: Float
+    multi :: Float,
+    year :: Int
   } deriving Generic
 instance FromJSON SavingInfo
 instance ToJSON SavingInfo
@@ -71,15 +72,8 @@ instance ToJSON RetireInfo
 data ResponseInfo = ResponseInfo 
   {
     leverage :: Float,
-    oneYear :: Float,
-    fiveYear :: Float,
-    tenYear :: Float,
-    fifteenYear :: Float,
-    twentyYear :: Float,
-    twentyFiveYear :: Float,
-    thrityYear :: Float,
-    thrityFiveYear :: Float,
-    fortyYear :: Float
+    indexYear :: [Int],
+    asset :: [Float]
   } deriving Generic
 instance ToJSON ResponseInfo
 
@@ -89,28 +83,57 @@ data ResponseRetireฺ = ResponseRetireฺ
     liveOnAge :: Float
   } deriving Generic
 instance ToJSON ResponseRetireฺ
+-- helper function
+calculateAsset :: (Fractional a, Enum a, Num b) => a -> a -> [b] -> a
+calculateAsset ans leverage list =  foldl (\acc cur -> ans + acc +((ans+acc)*(leverage/100)) ) 0 list
 
+dynamicYear year
+  | year >= 0   && year < 5  = 5
+  | year >= 5  && year < 10 = 10
+  | year >= 10 && year < 15 = 15
+  | year >= 15 && year < 20 = 20
+  | year >= 20 && year < 25 = 25
+  | year >= 25 && year < 30 = 30
+  | year >= 30 && year < 35 = 35
+  | year >= 35 && year < 40 = 40
+  | year >= 40 && year < 45 = 45
+  | year >= 45 && year < 50 = 50
+  | year >= 50 && year < 55 = 55
+  | year >= 55 && year < 60 = 60
+yearList :: (Num a, Enum a) => a -> [a]
+yearList amountY = [1..amountY]
+
+yearIndex year 
+  | year >= 0   && year < 5  = [1,5]
+  | year >= 5  && year < 10 = [1,5,10]
+  | year >= 10 && year < 15 = [1,5,10,15]
+  | year >= 15 && year < 20 = [1,5,10,15,20]
+  | year >= 20 && year < 25 = [1,5,10,15,20,25]
+  | year >= 25 && year < 30 = [1,5,10,15,20,25,30]
+  | year >= 30 && year < 35 = [1,5,10,15,20,25,30,35]
+  | year >= 35 && year < 40 = [1,5,10,15,20,25,30,35,40]
+  | year >= 40 && year < 45 = [1,5,10,15,20,25,30,35,40,45]
+  | year >= 45 && year < 50 = [1,5,10,15,20,25,30,35,40,45,50]
+  | year >= 50 && year < 55 = [1,5,10,15,20,25,30,35,40,45,50,55]
+  | year >= 55 && year < 60 = [1,5,10,15,20,25,30,35,40,45,50,55,60]
 
 -- Controller
 
 resultForInvestPlan :: SavingInfo -> ResponseInfo
-resultForInvestPlan res = ResponseInfo leverage' oneYear' fiveYear' tenYear' fifteenYear' twentyYear' twentyFiveYear' thrityYear' thrityFiveYear' fortyYear'
+resultForInvestPlan res = ResponseInfo leverage' year' asset'
 
   where 
         ans = foldl (\acc cur -> acc+ value res) 0 [1..12]
         leverage' = multi res
-        oneYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1]
-        fiveYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..5]
-        tenYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..10]
-        fifteenYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..15]
-        twentyYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..20]
-        twentyFiveYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..25]
-        thrityYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..30]
-        thrityFiveYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..35]
-        fortyYear' = foldl (\acc cur -> ans + acc +((ans+acc)*(leverage'/100)) ) 0 [1..40]
-
-
- 
+        -- helper function
+        handleAssetList :: [Int] -> [Float] -> [Float]
+        handleAssetList [] [] = []
+        handleAssetList [] result = result
+        handleAssetList (x:xs) result = [asset] ++ handleAssetList xs [asset]
+          where asset = calculateAsset ans leverage' $ yearList x
+        year' = yearIndex $ year res
+        asset' = handleAssetList year' []
+        
 resultForRetirePlan :: RetireInfo -> ResponseRetireฺ
 resultForRetirePlan res = ResponseRetireฺ totalAsset' liveOnAge'
 
